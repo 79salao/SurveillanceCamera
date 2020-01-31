c9c5f493709df60240c4a6d1cfe1ad4ffd21a868
Pedro Ojeda <darkpjo@gmail.com>
2020-01-31 12:53:48 +0100

    feature/verifyBothPasswords funcion para verificar que ambas contraseñas sean la misma

---
+++ Frontito/js/verificarCont.js
@@ -0,0 +1,14 @@
+function verificar(){
+
+    var p1 = document.getElementById('pass1');
+    var p2 = document.getElementById('pass2');
+
+    if( p1.value != p2.value){
+
+        console.log("nope")
+
+    }else{
+
+        console.log("yup")
+    }
+    }
