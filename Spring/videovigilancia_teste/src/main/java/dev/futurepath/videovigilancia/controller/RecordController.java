ba27aec7d62661be6f9f705e031eb28884d42f16
Angela Serrano <aserranocu@gmail.com>
2020-02-27 09:26:40 +0100

    Mejora de código en RecordController y nuevo compilado en jar

--- Spring/videovigilancia_teste/src/main/java/dev/futurepath/videovigilancia/controller/RecordController.java
+++ Spring/videovigilancia_teste/src/main/java/dev/futurepath/videovigilancia/controller/RecordController.java
@@ -3,12 +3,8 @@ package dev.futurepath.videovigilancia.controller;
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
-
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -32,88 +28,53 @@ public class RecordController {
 	
 	@RequestMapping(value= "/main")
 	public ModelAndView mainPage(HttpServletRequest request) throws ParseException{
-		ModelAndView mainModelAndView = new ModelAndView("html/mainpage");
-		ModelAndView redirectSignIn = new ModelAndView("redirect:/");
-		mainModelAndView.addObject("title","TB/O Records");
-		String from, to;		
-		if(request.getQueryString() != null) {
-			from = request.getQueryString().substring(5,15);
-			to = request.getQueryString().substring(19);
-		}else {
-			from = LocalDateTime.now().minusWeeks(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-			to = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-		}
-		mainModelAndView.addObject("record", recordDao.findRecordsByDates(from, to));
-		mainModelAndView.addObject("from", from);
-		mainModelAndView.addObject("to", to);
-		
-		/*String cookieUsername = (cookieID(request));
-		//Long cookieId = Long.parseLong(cookieID(request));
-		User user = userDao.findUserByUsername(cookieUsername);*/
-		User user = sharedMethods.findCookies(request);
-		mainModelAndView.addObject("user", user);
-		if(user==null) {
-			return redirectSignIn;
-		}
-		return mainModelAndView;
+		return modelView(request, "html/mainpage");
 	}
-	/*
-     * Método que devulve la página de charts. Incluye atributos for, to y records,
-     * que es un array de Dates.
-     */
+	
     @RequestMapping(value = "/charts")
     public ModelAndView chartsPage(HttpServletRequest request) {
-    	ModelAndView chartsModelAndView = new ModelAndView("html/statsPage");
-    	ModelAndView redirectSignIn = new ModelAndView("redirect:/");
-    	chartsModelAndView.addObject("title", "TB/O Charts");
-        String from, to;
-        if (request.getQueryString() != null) {
-            from = request.getQueryString().substring(5, 15);
-            to = request.getQueryString().substring(19);
-        } else {
-            from = LocalDateTime.now().minusWeeks(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            to = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        }
-        chartsModelAndView.addObject("records", recordDao.findDatesByDates(from, to));
-        chartsModelAndView.addObject("from", from);
-        chartsModelAndView.addObject("to", to);        
-        
-        User user = sharedMethods.findCookies(request);
-        chartsModelAndView.addObject("user", user); 
-        if(user==null) {
-			return redirectSignIn;
-		}
-        return chartsModelAndView;
+    	return modelView(request, "html/statsPage");
     }
-    //copypasteado
+
     @RequestMapping(value = "/charts2")
     public ModelAndView chartsPage2(HttpServletRequest request) {
-    	ModelAndView chartsModelAndView = new ModelAndView("html/statsPage2"); 
-    	ModelAndView redirectSignIn = new ModelAndView("redirect:/");
-    	chartsModelAndView.addObject("title", "TB/O Charts");
-        String from, to;
-        if (request.getQueryString() != null) {
-            from = request.getQueryString().substring(5, 15);
-            to = request.getQueryString().substring(19);
-        } else {
-            from = LocalDateTime.now().minusWeeks(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            to = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        }
-        
-        List<Date> records = recordDao.findDatesByDates(from, to);
-        chartsModelAndView.addObject("records", records);
-        for (Date date : records) {
-        	System.out.println(date);
-		}
-        
-        chartsModelAndView.addObject("from", from);
-        chartsModelAndView.addObject("to", to);  
-        User user = sharedMethods.findCookies(request);
-        chartsModelAndView.addObject("user", user);
-        if(user==null) {
-			return redirectSignIn;
-		}
-        return chartsModelAndView;
+    	return modelView(request, "html/statsPage2");
     }    
-
+    
+    private ModelAndView modelView(HttpServletRequest request, String direccion) {
+    	
+    	User user = sharedMethods.findCookies(request);
+    	if(user==null) {
+			return new ModelAndView("redirect:/");
+		}else {
+			String from, to;
+			ModelAndView modelAndView = new ModelAndView(direccion);	
+			if(request.getQueryString() != null) {
+				try {
+					from = request.getQueryString().substring(5,15);
+					to = request.getQueryString().substring(19);
+				}catch(StringIndexOutOfBoundsException ex) {
+					from = LocalDateTime.now().minusWeeks(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+					to = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+				}
+			}else{
+				from = LocalDateTime.now().minusWeeks(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+				to = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+			}
+			
+			if(direccion.equals("html/mainpage")) {
+				modelAndView.addObject("record", recordDao.findRecordsByDates(from, to));
+			}else{
+				List<Date> records = recordDao.findDatesByDates(from, to);
+				modelAndView.addObject("record", records);
+			}
+			
+			modelAndView.addObject("from", from);
+			modelAndView.addObject("to", to);
+			modelAndView.addObject("title","TB/O");
+			modelAndView.addObject("user", user);
+			return modelAndView;
+		}
+    }
+    
 }
